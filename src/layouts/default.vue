9ef2cd6b87a163c41f777c97265341aed0ad927d
Travis Reynolds <travis@travisreynolds.dev>
2022-05-01 16:22:19 +0100

    Update padding

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div bg="dark-500">
-    <div container="~" m="auto" h="min-screen" flex="~ col md:row" p="x-8 y-4">
-      <header w="md:1/3" m="auto" text="center">
+    <div container="~" m="auto" h="min-screen" flex="~ col md:row" p="x-8 y-4" gap="4">
+      <header w="md:1/3" m="auto" text="center" p="x-2">
         <h1 font="serif" text="uppercase 8xl md:6xl lg:8xl green-500">Wind</h1>
 
         <div m="t-6">
