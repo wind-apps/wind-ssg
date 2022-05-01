d154fa157deaaabf7bf637811b130cb8b3deba00
Travis Reynolds <travis@travisreynolds.dev>
2022-05-01 16:53:16 +0100

    Add some social stuff

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,15 @@ import 'virtual:windi.css'
 import '@fontsource/neuton'
 import '@fontsource/open-sans'
 
-export default defineApp({})
+export default defineApp({
+  socialTags: false,
+  head: {
+    title: 'Wind SSG',
+    link: [
+      {rel: 'icon', type: 'image/png', href: '/favicon.png' }
+    ],
+    meta: [
+      {name: 'description', content: 'A new static site generator, built with next-gen tooling' }
+    ]
+  }
+})
