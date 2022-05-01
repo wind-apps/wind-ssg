bff2a3e8738a85416e6f765a8da68e49771fb7a1
Travis Reynolds <travis@travisreynolds.dev>
2022-05-01 16:17:36 +0100

    Add mobile styles

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,20 +1,15 @@
 <template>
   <div bg="dark-500">
-    <div container="~" m="auto" h="min-screen" flex="~ row">
-      <header w="1/3" m="auto">
-        <h1 font="serif" text="uppercase 8xl green-500">Wind</h1>
+    <div container="~" m="auto" h="min-screen" flex="~ col md:row" p="x-8 y-4">
+      <header w="md:1/3" m="auto" text="center">
+        <h1 font="serif" text="uppercase 8xl md:6xl lg:8xl green-500">Wind</h1>
 
         <div m="t-6">
           <Header :title="$frontmatter.title" />
-
-          <nav flex="~ row" gap="4" m="t-6" font="bold" text="green-400 opacity-75">
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
-          </nav>
         </div>
       </header>
 
-      <div w="2/3" m="auto" h="auto" text="true-gray-400">
+      <div w="md:2/3" m="auto t-8 md:t-auto" h="auto" text="true-gray-400">
         <slot />
       </div>
     </div>
