dae0b028b7064928b276600739cf7692a0602770
Travis Reynolds <travis@travisreynolds.dev>
2022-05-01 16:22:13 +0100

    Add prism plugin

--- iles.config.ts
+++ iles.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'iles'
 import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
+  modules: ['@islands/prism'],
   vite: {
     plugins: [
       WindiCSS()
